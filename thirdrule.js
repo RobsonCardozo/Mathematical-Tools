9a1e925743c3c1165a5a124136f4e4fd5c0b5c2e
RobsonCardozo <robsonleaocardozo@hotmail.com>
2021-02-26 23:59:17 -0300

    initial commit

---
+++ thirdrule.js
@@ -0,0 +1,17 @@
+//The Third Rule
+
+//left number (first variable)
+let left_n = 569733;
+//right number (result of first variable)
+let right_n = 100;
+//question number
+let question = 683;
+//multiplication result
+let mult;
+//incognito number
+let x;
+
+mult = (right_n * question);
+x = (mult / left_n);
+
+console.log(x);
