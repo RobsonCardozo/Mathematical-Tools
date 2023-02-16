076afce396c2fe4a126519a24e6f38ffb44e84f1
robsoncardozo <robsonleaocardozo@hotmail.com>
2023-02-16 07:16:45 -0300

    rule_of_thirds implemented

--- rule_of_thirds.py
+++ rule_of_thirds.py
@@ -1,12 +1,37 @@
-#crescent (basic) state of rule of thirds
+import os
 
-left_n = float(input("Enter top left number: "))
-right_n = float(input ("Enter top right number: "))
-question = float(input ("Enter number in question: "))
-mult = float()
-x = float()
+def regra_de_tres(A, B, C):
+    D = (C * B) / A
+    return D
 
-mult = (right_n * question)
-x = (mult / left_n)
+while True:
+    try:
+        # Obter os valores A, B e C do usuário
+        A = float(input("Digite o valor de A: "))
+        B = float(input("Digite o valor de B: "))
+        C = float(input("Digite o valor de C: "))
 
-print(x)
+        # Verificar se os valores são todos diferentes de zero
+        if A == 0 or B == 0 or C == 0:
+            print("Erro: pelo menos um dos valores informados é igual a zero. Por favor, tente novamente.")
+            continue
+
+        # Calcular o valor de D utilizando a função regra_de_tres
+        D = regra_de_tres(A, B, C)
+
+        # Exibir o resultado para o usuário
+        print("O valor de D é:", D)
+
+    except ValueError:
+        # Caso o usuário digite um valor inválido, exibir mensagem de erro
+        print("Erro: um ou mais valores informados não são numéricos. Por favor, tente novamente.")
+        continue
+
+    # Perguntar se o usuário deseja reiniciar o programa
+    reiniciar = input("Deseja reiniciar o programa? (s/n): ")
+
+    if reiniciar.lower() == "s":
+        # Limpar a tela antes de reiniciar o programa
+        os.system("cls" if os.name == "nt" else "clear")
+    else:
+        break
